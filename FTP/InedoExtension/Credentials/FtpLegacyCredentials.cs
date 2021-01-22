97c74496341170ade6b62c32773f4fea11433d8f
Richard Hessinger <rhessinger@inedo.com>
2021-01-22 12:22:33 -0500

    - Migrated FTP to .NET 5 and SDK 1.10 - Updated ResourceCredentials to SecureResources and SecureCredentials

--- FTP/InedoExtension/Credentials/FtpLegacyCredentials.cs
+++ FTP/InedoExtension/Credentials/FtpLegacyCredentials.cs
@@ -3,16 +3,19 @@ using System.Security;
 using Inedo.Documentation;
 using Inedo.Extensibility;
 using Inedo.Extensibility.Credentials;
+using Inedo.Extensibility.SecureResources;
 using Inedo.Serialization;
 using Inedo.Web;
+using UsernamePasswordCredentials = Inedo.Extensions.Credentials.UsernamePasswordCredentials;
 
 namespace Inedo.Extensions.FTP.Credentials
 {
     [SlimSerializable]
-    [DisplayName("FTP")]
+    [DisplayName("(Legacy) FTP")]
     [Description("Username and password for the File Transfer Protocol")]
     [ScriptAlias("FTP")]
-    public sealed class FtpCredentials : ResourceCredentials
+    [PersistFrom("Inedo.Extensions.FTP.Credentials.FtpCredentials,FTP")]
+    public sealed class FtpLegacyCredentials : ResourceCredentials
     {
         [Persistent]
         [Required]
@@ -36,5 +39,17 @@ namespace Inedo.Extensions.FTP.Credentials
         {
             return new RichDescription("ftp://", new Hilite(this.UserName), "@", new Hilite(this.HostName));
         }
+
+        public override SecureCredentials ToSecureCredentials() => new UsernamePasswordCredentials
+        {
+            UserName = this.UserName,
+            Password = this.Password
+        };
+
+        public override SecureResource ToSecureResource() => new FtpSecureResource
+        {
+            HostName = this.HostName,
+            Port = this.Port
+        };
     }
 }
---
+++ FTP/InedoExtension/Credentials/FtpSecureResource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using Inedo.Documentation;
+using Inedo.Extensibility.SecureResources;
+using Inedo.Serialization;
+using UsernamePasswordCredentials = Inedo.Extensions.Credentials.UsernamePasswordCredentials;
+
+namespace Inedo.Extensions.FTP.Credentials
+{
+    [DisplayName("FTP Server")]
+    [Description("Host and port for the File Transfer Protocol")]
+    [Serializable]
+    public class FtpSecureResource : SecureResource
+    {
+        [Persistent]
+        [Required]
+        [DisplayName("FTP server")]
+        public string HostName { get; set; }
+
+        [Persistent]
+        [DefaultValue(21)]
+        public int Port { get; set; } = 21;
+
+        public override Type[] CompatibleCredentials => new[] { typeof(UsernamePasswordCredentials) };
+
+        public override RichDescription GetDescription()
+        {
+            return new RichDescription("ftp://", new Hilite(this.HostName));
+        }
+    }
+}
