b4981697afac3eb1bde339e15399efca18375c4c
Richard Hessinger <rhessinger@inedo.com>
2021-01-22 17:28:18 -0500

    Small tweak

--- FTP/InedoExtension/Properties/AssemblyInfo.cs
+++ FTP/InedoExtension/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Inedo.Extensibility;
 [assembly: AssemblyTitle("FTP")]
 [assembly: AssemblyDescription("Contains actions for communicating with a server using FTP.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2021")]
+[assembly: AssemblyVersion("1.10.0")]
+[assembly: AssemblyFileVersion("1.10.0")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
